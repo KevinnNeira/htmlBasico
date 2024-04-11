a665c507d153c532b4b8cf3fab4c1ebad3e769e3
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-04-11 08:03:34 -0500

    Terminado

---
+++ Dia5/main.js
@@ -0,0 +1,9 @@
+document.getElementById("Boton").addEventListener("click", function() {
+    var nombre = document.getElementById("nombre").value;
+    var edad = document.getElementById("edad").value;
+    var correo = document.getElementById("correo").value;
+
+    alert(nombre)
+    alert(edad)
+    alert(correo)
+});
